dcc36157b8ae26ff22d8eda97463d247ec7c4886
zhenshigang <zhenshigang@xdf.cn>
2023-11-30 17:16:57 +0800

    初始化开发

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # component-usage-webpack-plugin
 statistic component usage
+目前仅支持.vue和.js文件统计
+支持vue2和vue3
 
 ## npm install
 ```
@@ -55,4 +57,10 @@ module.exports = {
 
 | 参数 | 说明 | 类型 | 默认值 |
 | --- | --- | --- | --- |
-| regex | 正则表达式，用于匹配需要统计的组件 | RegExp | element-ui组件 |
+| regex | 正则表达式，用于匹配需要统计的组件 | RegExp | /<(el-[a-z-]+)/g |
+
+## 注意
+
+    1. 组件使用统计只在开发环境下生效,且统计启动时项目不会正常启动
+    2. 组件使用统计只统计.vue内的组件，无法统计动态创建的组件
+
--- index.js
+++ index.js
@@ -1,68 +1,96 @@
 // componentUsageWebpackPlugin.js
 const fs = require('fs')
 const path = require('path')
+const http = require('http')
+
 const express = require('express')
-const ejs = require('ejs')
+// const ejs = require('ejs')
 const vuecompiler = require('vue-template-compiler')
+const { parse } = require('@vue/compiler-sfc');
 const opn = require('opn');
 class componentUsageWebpackPlugin {
-  constructor (options) {
-    this.regex = options.regex
-  }
-
-  apply (compiler) {
-    const stats = {}
-
-    compiler.hooks.compilation.tap('componentUsageWebpackPlugin', (compilation) => {
-      compilation.hooks.normalModuleLoader.tap('componentUsageWebpackPlugin', (loaderContext, module) => {
-        // 只处理.vue文件
-        if (module.resource && module.resource.endsWith('.vue')) {
-        //   const source = module._source && module._source._value
-        // const source = module._source && module._source.source();
-          const source = fs.readFileSync(module.userRequest, 'utf-8')
-          //   console.log('source: ' + source, 'source')
-
-          if (source) {
-            // console.log(vuecompiler.parseComponent, vuecompiler.parseComponent(source), 'template')
-
-            // 使用vue-template-compiler解析.vue文件的<template>部分
-            const { template } = vuecompiler.parseComponent(source)
-
-            if (template && template.content) {
-              // 使用正则表达式查找所有以'el-'开头的Element UI组件
-            //   const regex = /<(el-[a-z-]+)/g
-              let match
-              while ((match = this.regex.exec(template.content)) !== null) {
-                console.log(match, 'mase')
-                const componentName = match[1]
-                stats[componentName] = (stats[componentName] || 0) + 1
-              }
-            }
-          }
+    constructor(options) {
+        const defaultOptions = {
+            regex: /<(el-[a-z-]+)/g
         }
-      })
-    })
+        this.options = Object.assign({}, defaultOptions, options)
+    }
 
-    compiler.hooks.done.tap('componentUsageWebpackPlugin', () => {
-      const statsArray = Object.entries(stats).sort((a, b) => b[1] - a[1]);
-      // 将统计结果写入到一个JSON文件中
-      const statsFile = path.resolve(__dirname, 'stats.json')
-      fs.writeFileSync(statsFile, JSON.stringify(statsArray))
+    apply(compiler) {
+        const stats = {}
 
-      // 启动一个服务器来显示统计结果
-      const app = express()
-      app.set('view engine', 'ejs')
-      app.set('views', path.resolve(__dirname, 'views')); // 设置视图目录
-      app.use(express.static(path.resolve(__dirname, 'public'))); // 托管静态文件
-      app.get('/', (req, res) => {
-        res.render('stats', { stats: statsArray })
-      })
-      app.listen(3006, () => {
-        console.log('Server is running at http://localhost:3006')
-        opn('http://localhost:3006')
-      })
-    })
-  }
+        compiler.hooks.compilation.tap('componentUsageWebpackPlugin', (compilation) => {
+            compilation.hooks.normalModuleLoader.tap('componentUsageWebpackPlugin', (loaderContext, module) => {
+
+                // 处理.vue文件
+                if (module.resource && module.resource.endsWith('.vue')) {
+                    const source = fs.readFileSync(module.userRequest, 'utf-8');
+                    let templateContent;
+                    try {
+                        // 尝试使用@vue/compiler-sfc解析.vue文件
+                        const { descriptor } = parse(source);
+                        templateContent = descriptor.template.content;
+                    } catch (error) {
+                        // 如果解析失败，使用vue-template-compiler解析.vue文件
+                        const { template } = vuecompiler.parseComponent(source);
+                        templateContent = template.content;
+                    }
+                    // 使用正则表达式查找所有的UI组件
+                    let match;
+                    while ((match = this.regex.exec(templateContent)) !== null) {
+                        const componentName = match[1];
+                        stats[componentName] = (stats[componentName] || 0) + 1;
+                    }
+                }
+                // 处理.js文件
+                else if (module.resource && module.resource.endsWith('.js')) {
+                    const source = fs.readFileSync(module.userRequest, 'utf-8');
+                    // 使用正则表达式查找所有的UI组件
+                    let match;
+                    while ((match = this.regex.exec(source)) !== null) {
+                        const componentName = match[1];
+                        stats[componentName] = (stats[componentName] || 0) + 1;
+                    }
+                }
+
+
+            })
+        })
+
+        compiler.hooks.done.tap('componentUsageWebpackPlugin', () => {
+            const statsArray = Object.entries(stats).sort((a, b) => b[1] - a[1]);
+            // 将统计结果写入到一个JSON文件中
+            const statsFile = path.resolve(__dirname, 'stats.json')
+            fs.writeFileSync(statsFile, JSON.stringify(statsArray))
+
+            // 启动一个服务器来显示统计结果
+            const app = express()
+            app.set('view engine', 'ejs')
+            app.set('views', path.resolve(__dirname, 'views')) // 设置视图目录
+            app.use(express.static(path.resolve(__dirname, 'public'))) // 托管静态文件
+            app.get('/', (req, res) => {
+                res.render('stats', { stats: statsArray })
+            })
+
+            let port = 30000
+            const server = http.createServer(app)
+            server.listen(port)
+            server.on('error', (error) => {
+                if (error.code === 'EADDRINUSE') {
+                    console.log(`Port ${port} is in use, trying another one...`)
+                    port++
+                    server.close()
+                    server.listen(port)
+                } else {
+                    throw error
+                }
+            })
+            server.on('listening', () => {
+                console.log(`Server is running at http://localhost:${port}`)
+                opn(`http://localhost:${port}`)
+            })
+        })
+    }
 }
 
 module.exports = componentUsageWebpackPlugin
