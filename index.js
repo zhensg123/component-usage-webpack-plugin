e724bd3243c7e7e11d025464019ce6fb2504de17
zhensg123 <xyqblog@163.com>
2023-12-03 14:34:00 +0800

    兼容webpack和webpack5

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const { mapJsFiles, mapVueFiles } = require('./src/mapFiles')
 const server = require('./src/server')
 const { objArrSort } = require('./src/util')
+const NormalModule = webpack.NormalModule;
+
 const initStatsMetric = function () {
     return {
         componentUsage: {}, // 统计UI 组件使用情况
@@ -20,42 +22,54 @@ class StatisticsWebpackPlugin {
         this.stats = initStatsMetric()
         this.id = 0
     }
-
+    switchCaseFile(module) {
+        if (this.id === 0) {
+            const { fileTypes } = this.options
+            const resource = module.resource
+            if(resource.indexOf('node_modules') !== -1){
+              return false
+            }
+            switch (fileTypes) {
+                // 处理.vue文件
+                case 'vue':
+                    if (/\.vue$/.test(resource)) {
+                        mapVueFiles(this, module);
+                    }
+                    break;
+                // 处理vue和js文件
+                case 'all':
+                    if (/\.js$/.test(resource)) {
+                        mapJsFiles(this, module);
+                    }
+                    if (/\.vue$/.test(resource)) {
+                        mapVueFiles(this, module);
+                    }
+                    break;
+                // 处理.jsx文件 
+            }
+        }
+    }
     apply(compiler) {
         console.log('\n正在分析文件...\n')
-        compiler.hooks.compilation.tap('componentUsageWebpackPlugin', (compilation) => {
-            compilation.hooks.normalModuleLoader.tap('componentUsageWebpackPlugin', (loaderContext, module) => {
-
-                if (this.id === 0) {
-                    const { fileTypes } = this.options
-                    const excludeNodeModules = module.resource ? module.resource.indexOf('node_modules') === -1 : false
-                    switch (fileTypes) {
-                        // 处理.vue文件
-                        case 'vue':
-                            if (/\.vue$/.test(module.resource) && excludeNodeModules) {
-                                mapVueFiles(this, module);
-                            }
-                            break;
-                        // 处理vue和js文件
-                        case 'all':
-                            if (/\.js$/.test(module.resource) && excludeNodeModules) {
-                                mapJsFiles(this, module);
-                            }
-                            if (/\.vue$/.test(module.resource) && excludeNodeModules) {
-                                mapVueFiles(this, module);
-                            }
-                            break;
-                        // 处理.jsx文件 
-                    }
-                }
-            })
+        compiler.hooks.compilation.tap('StatisticsWebpackPlugin', (compilation) => {
+            if (NormalModule.getCompilationHooks) {
+                // Webpack 5
+                NormalModule.getCompilationHooks(compilation).loader.tap('MyPlugin', (loaderContext, module) => {
+                    this.switchCaseFile(module)
+                });
+            } else {
+                // Webpack 4
+                compilation.hooks.normalModuleLoader.tap('MyPlugin', (loaderContext, module) => {
+                    this.switchCaseFile(module)
+                });
+            }
         })
 
-        compiler.hooks.done.tap('componentUsageWebpackPlugin', () => {
+        compiler.hooks.done.tap('StatisticsWebpackPlugin', (stats) => {
             // 将统计结果写入到一个JSON文件中
             // const statsFile = path.resolve(__dirname, 'stats.json')
             // fs.writeFileSync(statsFile, JSON.stringify(statsArray))
-
+            console.log(stats, 'statsvvv')
             if (this.id === 0) {
                 console.log('\n分析完成，正在生成统计结果...\n')
                 this.id++
